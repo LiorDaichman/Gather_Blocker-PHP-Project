7c04b969cacd69224b15f86825544f0cc0118c0c
shakedtur <59848826+shakedtur@users.noreply.github.com>
2020-12-01 00:03:36 +0200

    Add files via upload

---
+++ tests.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+require_once "funcs.php";
+
+
+class funcs_test extends PHPUnit_Framework_TestCase {
+    public function test_passwordCheck() {
+        $this->assertTrue( passwordCheck( "123re2w" ));
+		$this->assertFalse(passwordCheck( "78f3" ) );
+    }
+	public function test_availableUser(){
+		$this->assertTrue( availableUser( "lolsd3" ) );
+		$this->assertFalse(availableUser( "gunr121" ));
+		
+	}
+     public function test_passwordCheck_confirm() {
+        $this->assertTrue( passwordCheck_confirm( "123re2w","123re2w"));
+		$this->assertFalse(passwordCheck_confirm( "78f3","123re2w" ));
+    }
+	
+}
+
+
+
+
+
+
+?>
