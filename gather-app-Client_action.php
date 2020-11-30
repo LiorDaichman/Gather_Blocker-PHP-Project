3ad602a3a52f3a063e9b051f120c49f97e2eaf04
gunrcisus <73432669+gunrcisus@users.noreply.github.com>
2020-11-30 19:33:11 +0200

    30/11

---
+++ gather-app-Client_action.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href='https://fonts.googleapis.com/css?family=Baumans' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Black Han Sans' rel='stylesheet'>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+<link href='https://fonts.googleapis.com/css?family=Be Vietnam' rel='stylesheet'>
+<style>
+ul {
+  list-style-type: none;
+  padding:0;
+  margin:0;
+  background-color: #333;
+  
+  overflow: hidden;
+  top:0;
+  width:100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 7px 25px;
+  text-decoration: none;
+}
+
+li a:hover  {
+    background-color: Moccasin;
+   color:black;
+}
+.dropdown:hover .dropbtn{background-color: Moccasin;
+   color:black;
+   }
+
+li.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1;}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+
+
+h1{
+    margin-top:100px;
+ text-align:center;
+  font-family: "Sofia";
+  font-size: 48px;
+}
+h2{
+    text-align:center;   
+}
+body {
+  margin:0;
+  background-color:lightgreen;
+  font-family:'Be Vietnam';
+  text-align:left;
+  
+   
+}
+p{
+    margin-left:150px;
+   font-size: 34px; 
+}
+form{
+    margin:auto;
+    padding:20px 250px;
+    text-align:center; 
+}
+input[type=text],input[type=password] {
+  border: 2px solid grey;
+   background-color: #3CBC8D;
+  color: white;
+  border-radius: 4px;
+}
+input[type=text],input[type=password]:focus {
+  border: 3px solid #555;
+}
+input[type=submit]{
+
+border: 2px solid grey;
+  border-radius: 4px;
+}
+input[type=button], input[type=submit], input[type=reset] {
+  background-color: #3CBC8D;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-decoration: none;
+  margin: 4px 2px;
+  cursor: pointer;
+  border: 2px solid grey;
+   border-radius: 4px;
+   
+}
+#b1{ background-color: #3CBC8D;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-decoration: none;
+  margin: 4px 2px;
+  cursor: pointer;
+  border: 2px solid grey;
+   border-radius: 4px;
+   align:center;
+   margin-left:700px;
+   display:inline-block;}
+   
+select {
+display:block;
+  
+  margin:auto;
+  padding: 16px 20px;
+  border: none;
+  border-radius: 4px;
+  background-color: #f1f1f1;
+}
+
+@media screen and (max-width:800px) {
+li{width:100%;
+text-align:center;
+}
+li a{width:91%}
+h1 {
+    margin-top:200px;
+ text-align:center;
+  font-family: "Sofia";
+  font-size:35px;
+  }
+}
+table {
+	margin-left:200px;
+  width: 75%;
+  border-collapse: collapse;
+}
+
+table, td, th {
+  border: 1px solid black;
+  padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="gather-app-Worker.php" >Home</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">sign-up</a>
+    <div class="dropdown-content">
+      <a href="gather-customer-sign.php">customer</a>
+      <a href="gather-Worker-sign.php">employee</a>
+      <a href="gather-manager-sign.php">manager</a>
+    </div>
+  </li>
+  <li><a href="gather-contact.htm">Contact</a></li>
+  <li><a href="gather-about.htm">About</a></li>
+  <li><a href="gather-home.php">log-out</a></li>
+  <li><a href="#"><?php session_start();if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+  {echo "Welcome , " . $_SESSION['username'] . "!";} else {echo "Please log in first to see this page.";}?></a></li>
+   
+</ul>
+
+<h1><span style='font-size:10vw;'>&#9940;</span> gather blocker <span style='font-size:10vw;'>&#129298;</span></h1>
+<h2 style='font-family:Baumans'>"go out when it's safe" !</h2>
+
+<?php
+
+
+$q = $_POST['city'];
+
+$con = mysqli_connect('localhost','root','root','gather-blocker');
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
+
+
+$sql="SELECT `id`, `name`, `street`, `city`, `max-capacity`, `category`, `user-name`, `real-time` FROM `shops` WHERE `city`='$q'";
+
+$result = mysqli_query($con,$sql);
+
+
+echo "<table>
+<tr>
+<th>name</th>
+<th>street</th>
+<th>city</th>
+<th>max-capacity</th>
+<th>category</th>
+<th>real-time</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['street'] . "</td>";
+  echo "<td>" . $row['city'] . "</td>";
+  echo "<td>" . $row['max-capacity'] . "</td>";
+  echo "<td>" . $row['category'] . "</td>";
+  echo "<td>" . $row['real-time'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
+
+<button id="b1" onclick="document.location='gather-app-Client.php'">return to app</button>
+</body>
+</html>
