72fde5cb7308d7da8b49efcdb381b78795f2b8db
shakedtur <59848826+shakedtur@users.noreply.github.com>
2020-12-29 22:02:42 +0200

    29.12

--- gather-home.php
+++ gather-home.php
@@ -9,7 +9,150 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
 <link href='https://fonts.googleapis.com/css?family=Be Vietnam' rel='stylesheet'>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <style>
+h1{
+    margin-top:10px;
+ text-align:center;
+  font-family: "Sofia";
+  font-size: 48px;
+}
+h2{
+    text-align:center;   
+}
+
+
+p{
+    margin-left:150px;
+   font-size: 34px; 
+}
+body { margin:0;
+     background-color:lightgreen;
+    font-family:'Be Vietnam';
+	font-family: Arial, Helvetica, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #3CBC8D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  border-radius:4px;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+  position: relative;
+}
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color:PaleGreen;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+   border-radius:4px;
+  width: 35%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+  position: absolute;
+  right: 25px;
+  top: 0;
+  color: #000;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: red;
+  cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+  -webkit-animation: animatezoom 0.6s;
+  animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+  from {-webkit-transform: scale(0)} 
+  to {-webkit-transform: scale(1)}
+}
+  
+@keyframes animatezoom {
+  from {transform: scale(0)} 
+  to {transform: scale(1)}
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+
 ul {
   list-style-type: none;
   padding:0;
@@ -68,58 +211,19 @@ li.dropdown {
 .dropdown:hover .dropdown-content {
   display: block;
 }
-
-
-
-h1{
-    margin-top:10px;
- text-align:center;
-  font-family: "Sofia";
-  font-size: 48px;
-}
-h2{
-    text-align:center;   
-}
-body {
-  margin:0;
-  background-color:lightgreen;
-  font-family:'Be Vietnam';
-}
-p{
-    margin-left:150px;
-   font-size: 34px; 
-}
-form{
-    margin:auto;
-    padding:20px 250px;
-    text-align:center; 
-}
-input[type=text],input[type=password] {
-  border: 2px solid grey;
-   background-color: #3CBC8D;
-  color: white;
-  border-radius: 4px;
- 
-}
-input[type=text],input[type=password]:focus {
-  border: 3px solid #555;
-}
-input[type=submit]{
-
-border: 2px solid grey;
-  border-radius: 4px;
-}
-input[type=button], input[type=submit], input[type=reset]{
-  background-color: #3CBC8D;
+#myBtn{
+	
+	display:block;
+	background-color: #3CBC8D;
   border: none;
   color: white;
   padding: 16px 32px;
   text-decoration: none;
-  margin: 4px 2px;
+  margin: auto;
   cursor: pointer;
-  border: 2px solid grey;
+border: 2px solid grey;
+margin-top:100px;
 }
-
 select {
 display:block;
   
@@ -127,7 +231,7 @@ display:block;
   padding: 16px 20px;
   border: none;
   border-radius: 4px;
-  background-color: #f1f1f1;
+  background-color:#f1f1f1;
 }
 
 @media screen and (max-width:800px) {
@@ -136,20 +240,23 @@ text-align:center;
 }
 li a{width:91%}
 h1 {
-    margin-top:200px;
+    margin-top:20px;
  text-align:center;
   font-family: "Sofia";
   font-size:35px;
   }
+  /* Modal Content/Box */
+.modal-content {
+  background-color:PaleGreen;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+   border-radius:4px;
+  width:75%; /* Could be more or less, depending on screen size */
 }
-
-
+}
 </style>
 </head>
 <body>
-
-
-
 <ul>
   <li><a href="gather-home.php" ><i class="fa fa-home"></i> Home</a></li>
   <li class="dropdown">
@@ -168,37 +275,58 @@ h1 {
 <h2 style='font-family:Baumans'>"go out when it's safe" !</h2>
 
 
-<form action="home_action.php"target="_self" method="post" autocomplete="off">
-<fieldset>
-    <legend>Log-in:</legend>
-  <label for="user-name">user-name:
-  <i class="fas fa-user"></i>
-  </label><br>
-  <input type="text" id="user-name" name="user-name" required value=""><br>
-  <label for="password">password:
-  <i class="fas fa-lock"></i>
-  </label><br>
-  <input type="password" id="password" name="password" required value=""><br><br>
+
+
+<button onclick="document.getElementById('id01').style.display='block'" style="border-radius: 4px;width:auto;font-size: 32px; color:white;" id="myBtn">Log-in <i class="fas fa-user-lock"></i></button><br>
+<div id="id01" class="modal">
   
+  <form class="modal-content animate" action="home_action.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="avatar.png" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+          <label for="user-name">user-name:<i class="fas fa-user"></i>
+          </label><br>
+          <input type="text" id="user-name" name="user-name" required value="" placeholder="Enter Username" autocomplete="off"><br>
+           <label for="password">password:
+           <i class="fas fa-lock"></i>
+           </label><br>
+           <input type="password" id="password" name="password" required value="" placeholder="Enter password"><br><br>
+	
+
+  	  <label for="duty">authorization:</label><br>
+      <select id="duty" name="duty">
+      <option value="Manager">Manager</option>
+      <option value="Worker">Worker</option>
+      <option value="client">client</option>
+      </select>
   
-    <label for="duty">authorization:</label><br>
-   <select id="duty" name="duty">
-  <option value="Manager">Manager</option>
-  <option value="Worker">Worker</option>
-  <option value="client">client</option>
-  </select>
-  
-  <input id="button "type="submit" value="Submit">
-  </fieldset>
-  
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#3CBC8D">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
   </form>
-  
-  
- 
+</div>
 
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
 
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 
 </body>
 </html>
-
